96e2384ec04536449be441be38af7f3dfef105e6
ksugaevskaya <ksu.gaevskaya@gmail.com>
2024-12-16 15:31:44 -0600

    fix(basic-3.4): tag selectors

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="delete button";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.alt = "remove button with cross inside";
+    deleteButtonImg.className = "img";
     deleteButton.appendChild(deleteButtonImg);
 
 
